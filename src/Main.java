7d6a85e0afedfaf47300188f360c1f457271693b
amirhossein <amir_zaman8088@yahoo.com>
2024-12-13 23:04:12 +0330

    solve problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        System.out.println(maxDepth(new TreeNode()));
+    }
+
+    public static int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else if (root.left == null && root.right == null) {
+            return 1;
+        }
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        return leftDepth > rightDepth ? leftDepth + 1 : rightDepth + 1;
     }
 }
